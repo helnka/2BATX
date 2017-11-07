acbd589c746099722f7740737af33ed9348d3569
alumnahelnka <33466258+alumnahelnka@users.noreply.github.com>
2017-11-07 21:47:19 +0100

    Add files via upload

---
+++ sumanumeroslista_a1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+print "SUMA DE LOS NÚMEROS DE UNA LISTA"
+suma=0
+lista=[1,2,3,-5]
+for i in lista:
+	suma+=i
+print "La suma de los números de la lista es: ",suma
