60e51e2ed146bf80145f97c47e4b53ff4c0091b5
xaviics <33484634+xaviics@users.noreply.github.com>
2017-11-10 10:29:25 +0100

    Add files via upload

---
+++ xavileonardollistainversa.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+numero = int(input("Dígame cuántas palabras tiene la lista: "))
+if numero < 1:
+    print("¡Imposible!")
+else:
+	lista = []
+	for i in range(0,numero):
+		print "Dígame la palabra", (i+1)
+		palabra = raw_input()
+		lista += [palabra]
+		print("La lista creada es:", lista)
+		inversa = []
+		for i in lista:
+			inversa = [i] + inversa 
+print("La lista inversa es:", inversa)
