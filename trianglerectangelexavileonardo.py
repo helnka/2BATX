2374a8de6c389e890285cda9b4d3a4a2feb65ad0
xaviics <33484634+xaviics@users.noreply.github.com>
2017-11-14 11:28:11 +0100

    Add files via upload

---
+++ trianglerectangelexavileonardo.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+def rectangle (param1, param2, carcter):
+	for i in range (param1):
+		for i in range (param2):
+			print caracter,
+		print
+print("dime la amplaria")
+amplaria = input()
+print("dime la altura")
+altura = input()
+print("dime un caracter")
+caracter = raw_input()
+rectangle(altura,amplaria,caracter)
